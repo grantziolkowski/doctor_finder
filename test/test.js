aee8e5e0122f5baf8288ebd5182e14657661bfd0
grantziolkowski <grant.ziolkowski@gmail.com>
2015-09-22 16:52:34 -0400

    Configure testing chai, sinon, mocha, sinon-chai, testem testing environment

--- test/test.js
+++ test/test.js
@@ -1,9 +1,37 @@
-var assert = require("assert");
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
+if (typeof exports !== 'undefined' && this.exports !== exports) {
+
+  /*
+   * Here's why the node.js environment needs special
+   * treatment:
+   *
+   *   -  We need to identify dependencies so node.js
+   *      can load the necessary libraries. (In the
+   *      browser, these will be handled by explicit
+   *      includes, either of individual script files
+   *      or of concatenated, possibly minified versions.)
+   *
+   *   -  Node.js doesn't have a DOM into which we
+   *      can insert our views to test interactions.
+   *      We can simulate a DOM with jsdom.
+   *
+   */
+
+  global.jQuery = require("jquery");
+  global.$ = jQuery;
+  global.chai = require("chai");
+  global.sinon = require("sinon");
+  chai.use(require("sinon-chai"));
+
+  global.jsdom = require("jsdom").jsdom;
+  var doc = jsdom("<html><body></body></html>");
+  global.window = doc.createWindow();
+
+}
+
+var should = chai.should();
+
+describe("Application", function() {
+  it("creates a global variable for the name space", function () {
+    should.exist(App);
+  })
+})

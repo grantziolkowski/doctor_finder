9ab4a9e6b453765a2c3b75b90f46acb8d254666c
grantziolkowski <grant.ziolkowski@gmail.com>
2015-09-22 09:28:07 -0400

    Image shows inline with text

--- public/templates/show.js
+++ public/templates/show.js
@@ -1,13 +1,14 @@
 JST['templates/show'] = _.template(
-  "<div class='doctor_name'>\
-    <img class='img-circle' src=<%= imgUrl %> />\
+  "<div class='row' class='doctor_name'>\
+    <div class='col-med-1'>\
+      <img class='img-circle' src=<%= imgUrl %>>\
+    </div>\
+    <div class='col-xs-6'>\
     <ul class='doctor_info'>\
       <li><%= name %></li>\
       <li><%= streetAddress %></li>\
       <li><%= city %>, <%= state %> <%= zipCode %></li>\
     </ul>\
+    </div>\
   </div>"
-
-
-
 )
